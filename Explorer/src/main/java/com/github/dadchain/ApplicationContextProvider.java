f198e428e3986a0a2c5f155f2512032e09c6629a
bitking0522 <bitking0522@gmail.com>
2019-11-10 11:43:22 +0800

    feat: init main

---
+++ Explorer/src/main/java/com/github/dadchain/ApplicationContextProvider.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2018 The dad Authors
+ * This file is part of The dad library.
+ *
+ * The dad is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The dad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The dad.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.dadchain;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+
+@Component("applicationContextProviderExplorer")
+public class ApplicationContextProvider implements ApplicationContextAware {
+
+    private static ApplicationContext context;
+
+    private ApplicationContextProvider() {
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
+
+    public static <T> T getBean(String name, Class<T> aClass) {
+        return context.getBean(name, aClass);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(String name) {
+        return (T) context.getBean(name);
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        return context.getBean(clazz);
+    }
+
+    public static <T> T getProperty(String name, Class<T> clazz) {
+        final Environment environment = context.getEnvironment();
+        return environment.getProperty(name, clazz);
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return context;
+    }
+
+}
---
+++ Explorer/src/main/java/com/github/dadchain/ExplorerApplication.java
@@ -0,0 +1,20 @@
+package com.github.dadchain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@Slf4j
+@EnableAsync
+@EnableSwagger2
+@SpringBootApplication
+@MapperScan(basePackages = "com.github.dadchain.mapper")
+public class ExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExplorerApplication.class, args);
+    }
+}
