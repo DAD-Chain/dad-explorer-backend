4961deff5f448b3e0ea8c142b3fe8e15ad11b0d8
winnerKim0304 <winnerKim0304@gmail.com>
2019-11-27 17:12:19 +0800

    fix: release 1.3.0

--- Explorer/systemd_service/explorer_api_entrypoint.sh
+++ Explorer/systemd_service/explorer_api_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 install_path=$(cd `dirname $0`;pwd)
-jar_package=explorer-1.2.2.RELEASE.jar
+jar_package=explorer-1.3.0.RELEASE.jar
 cd ${install_path} && java -jar ${jar_package}
--- dadSynHandler/systemd_service/explorer_sync_entrypoint.sh
+++ dadSynHandler/systemd_service/explorer_sync_entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 install_path=$(cd `dirname $0`;pwd)
-jar_package=dadsynhandler-1.1.1-RELEASE.jar
+jar_package=dadsynhandler-1.3.0-RELEASE.jar
 cd ${install_path} && java -jar ${jar_package}
